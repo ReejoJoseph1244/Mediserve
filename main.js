bf91c205ae1e400b1b094360ca3bf0c88336d44e
Reejo Joseph <92742868+ReejoJoseph1244@users.noreply.github.com>
2022-11-01 01:04:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,59 @@ function showSlides(n){
 }
 
 // Auto Slide
+// Cart
+let cartIcon = document.querySelector('#cart-icon')
+let cart = document.querySelector('.cart')
+let closeCart = document.querySelector('#close-cart')
+//To open cart
+cartIcon.onclick = () =>{
+    cart.classList.add("active");
+};
+//To close cart
+closeCart.onclick = () =>{
+    cart.classList.remove("active");
+};
+
+//Cart working Js
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded',ready);
+}
+else{
+    ready();
+}
+
+function ready(){
+    //Remove iteams
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for(var i=0;i<removeCartButtons.length;i++)
+    {
+        var button = removeCartButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+function updatetotal(){
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    var cartBoxes = cartContent.getElementsByClassName('cart-box')
+    var total=0;
+    for(var i=0;i<cartBoxes.length;i++){
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var price = parseFloat(priceElement.innerText.replace('₹',""));
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var quantity = quantityElement.value;
+
+        total = total + (price*quantity);
+
+        document.getElementsByClassName('total-price')[0].innerHTML = '₹' + total;
+
+
+    }
+}
